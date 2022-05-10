98a5bdc53e11e61ddb9ecd4f56f0f683c8d10c60
kod-x <olamilekanadeleke123@gmail.com>
2022-05-10 19:52:12 +0100

    update

--- lib/features/authentication/views/screens/intro_screen.dart
+++ lib/features/authentication/views/screens/intro_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:vigo_place/cores/components/custom_scaffold_widget.dart';
 
 class IntroScreen extends StatelessWidget {
-  const IntroScreen({ Key? key }) : super(key: key);
+  const IntroScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +10,6 @@ class IntroScreen extends StatelessWidget {
       body: Column(
         children: <Widget>[],
       ),
-      
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_screen_utils/responsive_screenutil_init.dart';
 
 import 'cores/constants/color.dart';
 import 'cores/navigator/app_router.dart';
@@ -6,21 +7,25 @@ import 'cores/navigator/router_generator.dart';
 import 'features/authentication/views/screens/auth_state_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'VigoPlace',
-      theme: ThemeData(primaryColor: kcPrimaryColor),
-      initialRoute: AuthStateScreen.route,
-      onGenerateRoute: RouteGenerator.generateRoute,
-      navigatorKey: AppRouter.instance.navigatorKey,
+    return ResponsiveScreenUtilInit(
+      designSize: const Size(375, 812),
+      child: MaterialApp(
+        title: 'VigoPlace',
+        theme: ThemeData(primaryColor: kcPrimaryColor),
+        initialRoute: AuthStateScreen.route,
+        onGenerateRoute: RouteGenerator.generateRoute,
+        navigatorKey: AppRouter.instance.navigatorKey,
+      ),
     );
   }
 }
